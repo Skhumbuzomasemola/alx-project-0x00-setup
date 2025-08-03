456c1a3948c1e2140855b0331fc7524e4b68f41e
Skhumbuzomasemola <skhumbuzomasemola13@gmail.com>
2025-08-03 23:11:46 +0200

    Update index.tsx

--- alx-project-0x00/pages/index.tsx
+++ alx-project-0x00/pages/index.tsx
@@ -1,4 +1,13 @@
+const Home: React.FC = () => {
+  return (
+    <main className="flex flex-col h-screen justify-center items-center text-4xl font-semibold">
+      <h1>Airbnb Application Clone system</h1>
+      <button className=" border px-3 py-1 text-lg mt-3  bg-blue-500 text-white rounded-full">Get Started</button>
+    </main>
+  )
+}
 
+export default Home;
     <div
       className={`${geistSans.className} ${geistMono.className} font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20`}
     >
