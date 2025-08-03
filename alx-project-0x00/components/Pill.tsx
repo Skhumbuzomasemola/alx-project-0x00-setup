d2b5409bacacd743e864b31071085f8c66e3b1c4
Skhumbuzomasemola <skhumbuzomasemola13@gmail.com>
2025-08-03 23:54:23 +0200

    Update Pill.tsx

--- alx-project-0x00/components/Pill.tsx
+++ alx-project-0x00/components/Pill.tsx
@@ -1,9 +1,2 @@
-const Pill: React.FC = () => {
-  return (
-    <div className=" flex justify-center  items-center bg-[#F9F9F9] px-3 w-auto h-[27px] rounded-full">
-      <p className=" text-sm ">Title</p>
-    </div>
-  )
-}
+import { PillProps } from "@/interfaces";
 
-export default Pill;
