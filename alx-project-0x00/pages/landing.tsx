d45f79a0ad22626c0e41f8086ce738f75c9bd1bb
Skhumbuzomasemola <skhumbuzomasemola13@gmail.com>
2025-08-04 00:11:42 +0200

    Update landing.tsx

--- alx-project-0x00/pages/landing.tsx
+++ alx-project-0x00/pages/landing.tsx
@@ -8,6 +8,7 @@ const Landing: React.FC =  () => {
       <Button title="Small" styles="rounded-sm" />
 <Button title="Medium" styles="rounded-md" />
 <Button title="Full" styles="rounded-full" />
+      <Button title="Large" styles="rounded-lg" />
     </div>
   )
 }
